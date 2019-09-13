f43618e95a9365d39041e54682586e47a93f965c
Gukhwa Jeon <g.jeon3743@gmail.com>
2019-09-13 18:36:17 +0200

    refactored. middle ware body parser used

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
-
 const fs = require('fs');
-const qs = require('querystring');
 const path = require('path');
 
 const port = 3000;
 const dirData = "./data";
 const template = require('./lib/template.js');
+
 const sanitizeHtml = require('sanitize-html');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (request, response) {
-
   fs.readdir(dirData, function (error, fileList) {
     const title = "Welcome";
     const description = "Hello Node.js";
@@ -23,11 +23,9 @@ app.get('/', function (request, response) {
     const html = template.html(title, list, body, control);
     response.send(html);
   });
-
 });
 
 app.get('/page/:pageId', function (request, response) {
-
   const title = request.params.pageId;
   const filteredTitle = path.parse(title).base;
 
@@ -37,12 +35,10 @@ app.get('/page/:pageId', function (request, response) {
       const sanitizedDescription = sanitizeHtml(description, {
         allowedTags: ['h1']
       });
-
       const list = template.list(fileList);
       const control = `
       <a href="/create">create</a>
       <a href="/update/${sanitizedTitle}">update</a>
-
       <form action="/delete_process" method="POST"
       onSubmit='return confirm("are you sure that you want to delete?")'>
         <input type="hidden" name="title" value="${sanitizedTitle}">
@@ -54,11 +50,9 @@ app.get('/page/:pageId', function (request, response) {
       response.send(html);
     });
   });
-
 });
 
 app.get('/create', function (request, response) {
-
   fs.readdir(dirData, function (error, fileList) {
     const title = "WEB - create";
     const body = `
@@ -73,39 +67,26 @@ app.get('/create', function (request, response) {
     const html = template.html(title, list, body, control);
     response.send(html);
   });
-
 });
 
 app.post('/create_process', function (request, response) {
+  const post = request.body;
+  const postTitle = post.title;
+  const postDescription = post.description;
 
-  let body = ``;
-
-  request.on('data', function (data) {
-    body += data;
-  });
-
-  request.on('end', function () {
-    const post = qs.parse(body);
-    const postTitle = post.title;
-    const postDescription = post.description;
-
-    //  user input is empty
-    if (!postTitle || !postDescription) {
-      response.send("fill the information");
-    } else {
-      var filteredPostTitle = path.parse(postTitle).base;
-      fs.writeFile(`${dirData}/${filteredPostTitle}`, postDescription, 'utf8', function (err) {
-      });
-      response.redirect(301, `/page/${filteredPostTitle}`);
-    }
-  });
-
+  //  user input is empty
+  if (!postTitle || !postDescription) {
+    response.send("fill the information");
+  } else {
+    var filteredPostTitle = path.parse(postTitle).base;
+    fs.writeFile(`${dirData}/${filteredPostTitle}`, postDescription, 'utf8', function (err) {
+    });
+    response.redirect(301, `/page/${filteredPostTitle}`);
+  }
 });
 
 app.get('/update/:pageId', function (request, response) {
-
   const title = request.params.pageId;
-
   fs.readFile(`${dirData}/${title}`, "utf8", function (err, description) {
     fs.readdir(dirData, function (error, fileList) {
       const list = template.list(fileList);
@@ -122,55 +103,34 @@ app.get('/update/:pageId', function (request, response) {
       response.send(html);
     });
   });
-
 });
 
 app.post('/update_process', function (request, response) {
+  const post = request.body;
+  const postTitleOrigin = post.title_origin;
+  const postTitleUpdated = post.title_updated;
+  const postDescription = post.description;
 
-  let body = ``;
-
-  request.on('data', function (data) {
-    body += data;
-  });
-
-  request.on('end', function () {
-    const post = qs.parse(body);
-    const postTitleOrigin = post.title_origin;
-    const postTitleUpdated = post.title_updated;
-    const postDescription = post.description;
-
-    //  user input is empty
-    if (!postTitleUpdated || !postDescription) {
-      response.send("fill the information");
-    } else {
-      const filteredPostTitleUpdated = path.parse(postTitleUpdated).base;
-      fs.rename(`${dirData}/${postTitleOrigin}`, `${dirData}/${filteredPostTitleUpdated}`, function (err) {
-      });
-      fs.writeFile(`${dirData}/${filteredPostTitleUpdated}`, postDescription, 'utf8', function (err) {
-      });
-      response.redirect(301, `/page/${filteredPostTitleUpdated}`);
-    }
-  });
-
+  //  user input is empty
+  if (!postTitleUpdated || !postDescription) {
+    response.send("fill the information");
+  } else {
+    const filteredPostTitleUpdated = path.parse(postTitleUpdated).base;
+    fs.rename(`${dirData}/${postTitleOrigin}`, `${dirData}/${filteredPostTitleUpdated}`, function (err) {
+    });
+    fs.writeFile(`${dirData}/${filteredPostTitleUpdated}`, postDescription, 'utf8', function (err) {
+    });
+    response.redirect(301, `/page/${filteredPostTitleUpdated}`);
+  }
 });
 
 app.post('/delete_process', function (request, response) {
-
-  let body = ``;
-
-  request.on('data', function (data) {
-    body += data;
+  const post = request.body;
+  const postTitle = post.title;
+  const filteredPostTitle = path.parse(postTitle).base;
+  fs.unlink(`${dirData}/${filteredPostTitle}`, function (err) {
   });
-
-  request.on('end', function () {
-    const post = qs.parse(body);
-    const postTitle = post.title;
-    const filteredPostTitle = path.parse(postTitle).base;
-    fs.unlink(`${dirData}/${filteredPostTitle}`, function (err) {
-    });
-    response.redirect(301, `/`);
-  });
-
+  response.redirect(301, `/`);
 });
 
 app.listen(port, function () {
